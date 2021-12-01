b81175c24fdd817c7fc131e417150b903fc6b375
amelisamuel <67370559+amelisamuel@users.noreply.github.com>
2021-12-01 21:37:54 +0100

    peaches

---
+++ src/components/Portfolio.vue
@@ -0,0 +1,78 @@
+<template>
+    <section class="bg-light" id="portfolio">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Portfolio</h2>
+            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+          </div>
+        </div>
+        <div class="row">
+          
+            <div class="col-md-4 col-sm-6 portfolio-item" v-for="(obj, key) in portfolioJSON" i=key :key="key">
+                <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+                    <div class="portfolio-hover">
+                        <div class="portfolio-hover-content">
+                            <i class="fas fa-plus fa-3x"></i>
+                        </div>
+                    </div>
+                    <img class="img-fluid" src="../assets/img/portfolio/1.jpg" alt="">
+                </a>
+                <div class="portfolio-caption">
+                    <h4>{{ obj.caption }}</h4>
+                    <p class="text-muted">{{ obj.title }}</p>
+                </div>
+            </div>        
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+/* eslint-disable */
+// eslint-disable-next-line
+
+export default {
+    data: () => ({
+        portfolioJSON: [
+            {
+                img: '../assets/img/portfolio/1.jpg',
+                caption: 'Explore',
+                title: 'Graphic Design'
+            },
+            {
+                img: '../assets/img/portfolio/2.jpg',
+                caption: 'Finish',
+                title: 'Identify'
+            },
+            {
+                img: '../assets/img/portfolio/3.jpg',
+                caption: 'Lines',
+                title: 'Branding'
+            },
+            {
+                img: '../assets/img/portfolio/4.jpg',
+                caption: 'Southwest',
+                title: 'Website Design'
+            },
+            {
+                img: '../assets/img/portfolio/5.jpg',
+                caption: 'Window',
+                title: 'Photography'
+            },
+            {
+                img: '../assets/img/portfolio/6.jpg',
+                caption: 'Coffee',
+                title: 'Drink a Lot!' 
+            },
+           
+        ]
+    })
+}
+</script>
+
+<style lang="css">
+
+</style>
+
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/css/styles.css'
+import './assets/css/bootstrap.css'
 
 createApp(App).use(router).mount('#app')
